7052ca9d0362a68e1750fb7201799c28cc673093
unknown <mrunalp.spaceo@gmail.com>
2024-03-04 16:15:33 +0530

    add to cart and register and login page

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const port = process.env.port || 8000   // basicaly agar apde dynamic ke pacchi 
 
 app.use(express.static('public')) // basicaly a ke 6 ke a badha assest kya agd available 6
 
+
+//configure  /  set template engine
+app.use(expressLayouts)
+app.set ('views',path.join(__dirname , '/resources/views'))
+app.set ('view engine','ejs')
+// routes karta phela j av vu joi a
+
+
 app.get('/', (req,res)=>{
 
     
@@ -16,10 +24,29 @@ app.get('/', (req,res)=>{
     //res.send("hello from server");
 })
 
-//configure  /  set template engine
-app.use(expressLayouts)
-app.set ('views',path.join(__dirname , '/resources/views'))
-app.set ('view engine','ejs')
+app.get('/cart', (req,res)=>{
+
+    
+    res.render('customer/cart');  // views sudhi no path phelethi define hoy 6 atle sidho a 
+    //res.send("hello from server");
+})
+
+app.get('/login', (req,res)=>{
+
+    
+    res.render('auth/login');  // views sudhi no path phelethi define hoy 6 atle sidho a 
+    //res.send("hello from server");
+})
+
+
+app.get('/register', (req,res)=>{
+
+    
+    res.render('auth/register');  // views sudhi no path phelethi define hoy 6 atle sidho a 
+    //res.send("hello from server");
+})
+
+
 
 
 
